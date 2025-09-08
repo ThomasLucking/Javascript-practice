feb23e77c58707e3e0e776affa8b98197288a20a
thomaslucking <telucking@gmail.com>
2025-09-08 09:09:13 +0200

    Finished palindrome project

--- Palindrome-Checker/scripts.js
+++ Palindrome-Checker/scripts.js
@@ -1,31 +1,30 @@
 const checkbutton = document.getElementById("check-btn")
 const inputvalue = document.getElementById("text-input")
+const answer = document.getElementById("result")
 
 
-// this reverses a string.
-/*let s = "GeeksforGeeks";
-const ans = [...s].reverse().join("");
-console.log(ans);*/
-
-
-const cleanedinputvalue = (str) =>{
-    const regex = /[^a-zA-Z0-9]/
-    return str.replace(regex, "");
+const cleanedinputvalue = (str) => {
+    const regex = /[^a-zA-Z0-9]/g
+    return str.replace(regex, "").toLowerCase();
 }
 
 
-const reversestring = (inputvalue) =>{
-    return reversedstring = [...currVal].reverse().join("");
+const reversestring = (str) => {
+    return [...str].reverse().join("");
 }
 
 checkbutton.addEventListener("click", () => {
     if (inputvalue.value === "") {
         alert("Please input a value");
-    }else{
-        let item = inputvalue.value
-        const currVal = cleanInputString(item.value);
-        const reversedstring = [...currVal].reverse().join("");
-        console.log(reversedstring);
-
+    } else {
+        const cleaned = cleanedinputvalue(inputvalue.value);
+        const reversed = reversestring(cleaned);
+        if (reversed !== cleaned) {
+            result.style.display = "block";
+            result.innerText = `${inputvalue.value} is not a palindrome`;
+        } else {
+            result.style.display = "block";
+            result.innerText = `${inputvalue.value} is a palindrome`;
+        }
     }
 });
---
+++ Random-js/whatever.js
@@ -0,0 +1,4 @@
+// this reverses a string.
+let s = "Thomas";
+const ans = [...s].reverse().join("");
+console.log(ans);
