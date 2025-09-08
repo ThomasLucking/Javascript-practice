e641fc435ab02510769ec6fa14b3865a6b42d1a3
Thomas Lucking <telucking@gmail.com>
2025-09-08 07:46:12 +0200

    new changes

--- Palindrome-Checker/scripts.js
+++ Palindrome-Checker/scripts.js
@@ -8,17 +8,24 @@ const ans = [...s].reverse().join("");
 console.log(ans);*/
 
 
+const cleanedinputvalue = (str) =>{
+    const regex = /[^a-zA-Z0-9]/
+    return str.replace(regex, "");
+}
 
 
 const reversestring = (inputvalue) =>{
-    return str.split("").reverse().join("");
+    return reversedstring = [...currVal].reverse().join("");
 }
 
 checkbutton.addEventListener("click", () => {
     if (inputvalue.value === "") {
         alert("Please input a value");
     }else{
-        let value = inputvalue
-        const cleanedinputvalue = str.replace(/[^a-zA-Z0-9]/g, "");
+        let item = inputvalue.value
+        const currVal = cleanInputString(item.value);
+        const reversedstring = [...currVal].reverse().join("");
+        console.log(reversedstring);
+
     }
 });
