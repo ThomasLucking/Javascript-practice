34721b6ef077a3bd4104e0494f6762cb0e6f2536
thomaslucking <telucking@gmail.com>
2025-09-12 15:16:23 +0200

    Finished FFC

---
+++ telephone-validator/script.js
@@ -0,0 +1,25 @@
+1 
+const input = document.getElementById("user-input")
+const results = document.getElementById("results-div")
+const checkbuton = document.getElementById("check-btn")
+const clearbutton = document.getElementById("clear-btn")
+
+
+checkbuton.addEventListener('click', () => {
+    if (input.value === "") {
+        alert("Please provide a phone number")
+    }else{
+        // Matches a valid US phone number with optional country code proper area code and optional spaces or dashes.
+        const phone_number_regex_list = [/^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/]
+        const isValid = (msg) => phone_number_regex_list.some((regex) => regex.test(msg));
+        // the isValid list return true if atleast one regex matches in that list.
+        results.textContent = isValid(input.value)
+            ? `Valid US number: ${input.value} `
+            : `Invalid US number: ${input.value}`; 
+    }
+});
+
+clearbutton.addEventListener('click', () => {
+    results.innerText = "";
+});
+
