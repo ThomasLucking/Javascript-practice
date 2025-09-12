bc2cab1150bd021a15e886c8db1877ad33779b4d
thomaslucking <telucking@gmail.com>
2025-09-12 15:48:11 +0200

    New changes

--- telephone-validator/script.js
+++ telephone-validator/script.js
@@ -1,4 +1,4 @@
-1 
+1 // grabs each element from their ID
 const input = document.getElementById("user-input")
 const results = document.getElementById("results-div")
 const checkbuton = document.getElementById("check-btn")
@@ -11,14 +11,16 @@ checkbuton.addEventListener('click', () => {
     }else{
         // Matches a valid US phone number with optional country code proper area code and optional spaces or dashes.
         const phone_number_regex_list = [/^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/]
-        const isValid = (msg) => phone_number_regex_list.some((regex) => regex.test(msg));
         // the isValid list return true if atleast one regex matches in that list.
+        const isValid = (msg) => phone_number_regex_list.some((regex) => regex.test(msg));
+        
+        // tenary operator to check if the it's a valid US number or not.
         results.textContent = isValid(input.value)
             ? `Valid US number: ${input.value} `
             : `Invalid US number: ${input.value}`; 
     }
 });
-
+// when the clear button it clicked it resets the innertext of the results div
 clearbutton.addEventListener('click', () => {
     results.innerText = "";
 });
